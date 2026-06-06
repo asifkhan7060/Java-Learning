209b2d72ec967aac5fbd1e3a2be18041fc4b10d6
asifkhan7060 <studiesbav.asifkhan@gmail.com>
2026-06-06 22:11:35 +0530

    OOPs small update

--- 04-OOPs/src/B_ClassAndObject/Way1/CarObjects.java
+++ 04-OOPs/src/B_ClassAndObject/Way1/CarObjects.java
@@ -8,21 +8,23 @@ package B_ClassAndObject.Way1;
 public class CarObjects {
     public static void main() {
 
-        // Creating car object
-        CarClass car1 = new CarClass();
+        // Creating and instantiating an object
+                CarClass car1 = new CarClass();  // new CarClass() -> Process of Instantiation
 
-        // Process of Instantiation
-        car1.brand = "BMW";
-        car1.colour = "Red";
-        car1.speed = 89;
+        // Assigning values to instance variables
+                car1.brand = "BMW";   // car1 = reference variable, brand = instance variable
+                car1.colour = "Red";
+                car1.speed = 89;
 
-        car1.drive();
+        // Calling an instance method
+                car1.drive();
 
 
-        // Creating another car object
-        CarClass car2 = new CarClass();
+        // Creating and instantiating an object
+        CarClass car2;
+        car2 = new CarClass();
 
-        // Process of Instantiation
+        // Assigning values to instance variables
         car2.brand = "Audi";
         car2.colour = "Green";
         car2.speed = 100;
--- 04-OOPs/src/B_ClassAndObject/Way2/ClassAndObject.java
+++ 04-OOPs/src/B_ClassAndObject/Way2/ClassAndObject.java
@@ -29,3 +29,24 @@ public class ClassAndObject {
 
 
 }
+
+// Rule to Note : In Java, if a class is declared as public, the file name must be exactly the same as the public class name.
+
+/**
+
+public class XYZ {         Error : java: class XYZ is public, should be declared in a file named XYZ.java
+    public static void main() {
+
+        // Creating car object
+        ClassAndObject car1 = new ClassAndObject();
+
+        // Process of Instantiation
+        car1.brand = "BMW";
+        car1.colour = "Red";
+        car1.speed = 100;
+
+        car1.drive();
+    }
+}
+
+ */
